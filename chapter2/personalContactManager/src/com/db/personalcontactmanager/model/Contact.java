a7118c93c952f57d61090549e7473ba2b610b6cd
sunnykraditya@gmail.com <sunnykraditya@gmail.com@cc473cde-9342-eec6-04f4-a1abff5fce9c>
2014-04-15 15:41:00 +0000

    #data model

---
+++ chapter2/personalContactManager/src/com/db/personalcontactmanager/model/Contact.java
@@ -0,0 +1,39 @@
+package com.db.personalcontactmanager.model;
+
+public class Contact {
+
+	private long id;
+	private String name, contactNo, email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContactNo() {
+		return contactNo;
+	}
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
